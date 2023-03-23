d998b55b0877284720b145f068109a2c918d996d
Emily Miles <emilymiles193@gmail.com>
2023-03-22 21:45:33 -0400

    worked on API

--- javascript.js
+++ javascript.js
@@ -1,10 +1,20 @@
+var cityInput =$("input");
+console.log(cityInput);
 let weather={
-"apiKey": "9258a0385e244fedfb9b649c2c90053a",
-fetchWeather: function(){
-fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=2bdcf4a80dd9deb5726ef62daec520a9&units=imperial"
-)
-.then((response)=> response.json())
-.then((data)=>console.log(data))
-}
+fetchWeather: function () {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        Denver +
+        "&units=metric&appid=" +
+        "9258a0385e244fedfb9b649c2c90053a"
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => console.log(data));
+  }
 }
