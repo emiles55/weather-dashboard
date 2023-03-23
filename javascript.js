7eca41012da8a903ac9b3bce964d9073b14ff2a5
Emily Miles <emilymiles193@gmail.com>
2023-03-22 21:14:13 -0400

    worked on weather API

--- javascript.js
+++ javascript.js
@@ -1,2 +1,10 @@
-import {myApi} from './api'
-const apiKey=myApi();
+let weather={
+"apiKey": "9258a0385e244fedfb9b649c2c90053a",
+fetchWeather: function(){
+fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=2bdcf4a80dd9deb5726ef62daec520a9&units=imperial"
+)
+.then((response)=> response.json())
+.then((data)=>console.log(data))
+}
+}
