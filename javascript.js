49f4b489b93facd0234983a676edf97754ab9662
Emily Miles <emilymiles193@gmail.com>
2023-03-26 11:03:16 -0400

    worked on javascript

--- javascript.js
+++ javascript.js
@@ -1,29 +1,24 @@
-var cityInput =$("input");
-console.log(cityInput);
-let weather={
-fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        Denver +
-        "&units=imperial&appid=" +
-        "9258a0385e244fedfb9b649c2c90053a"
-    )
-      .then((response) => {
-        if (!response.ok) {
-          alert("No weather found.");
-          throw new Error("No weather found.");
-        }
-        return response.json();
-      })
-      .then((data) => console.log(data));
-  }
-}
-function renderWeather(){
+
+
+function renderWeather(weather){
+  city=document.getElementById("#location");
+  temp=document.getElementById("#degrees");
+  city.textContent=weather.name;
+  temp.textContent=weather.main.temp;
 
 }
 function fetchWeather(query){
-var url= "https://api.openweathermap.org/data/2.5/weather?q=" +
-query +
+var url= "https://api.openweathermap.org/data/2.5/weather?q=" +"Denver"+
 "&units=imperial&appid=" +
-"9258a0385e244fedfb9b649c2c90053a"
+"9258a0385e244fedfb9b649c2c90053a";
+fetch(url)
+.then((response) => {
+  if (!response.ok) {
+    alert("No weather found.");
+    throw new Error("No weather found.");
+  }
+  return response.json();
+})
+.then((data) => console.log(data))
 }
+fetchWeather();
